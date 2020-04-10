8e38e3634d1e86eaf402726db489c9e277b8dbe3
asimonv <aasimon@uc.cl>
2020-04-10 14:01:05 -0400

    Updates

---
+++ precache-manifest.df572bae77d42e6721cb5767aa282d80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94cced1c6172c525ea9d928d8c56c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69b016ad321dc2222c1f",
+    "url": "/static/css/main.6edf94dc.chunk.css"
+  },
+  {
+    "revision": "2f3fd00ec4e5fadcf970",
+    "url": "/static/js/2.f2b1cc68.chunk.js"
+  },
+  {
+    "revision": "69b016ad321dc2222c1f",
+    "url": "/static/js/main.4918ca59.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "56b78509cf4402c13cd80889185ed7de",
+    "url": "/static/media/nyt-webfont.56b78509.woff"
+  },
+  {
+    "revision": "66cc4e833ac3d7e8b623882d2e4221ec",
+    "url": "/static/media/nyt-webfont.66cc4e83.woff2"
+  },
+  {
+    "revision": "373bef8dbc1a357c1006e8d42154db9f",
+    "url": "/static/media/nyt.373bef8d.ttf"
+  }
+]);
